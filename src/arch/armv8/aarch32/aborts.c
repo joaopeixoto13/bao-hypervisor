53448ccfd6e1b2a63aaaedd730d1991b3259a630
João Peixoto <joaopeixotooficial@gmail.com>
2026-09-22 10:28:58 +0100

    fix(armv8): label registers correctly in the aarch32 internal abort dump
    SAVE_HYP_GPRS pushes r0-r12 and then the pre-exception sp and lr, so
    the frame starts with sp and lr followed by r0 to r12. The dump walked
    it backwards and printed sp as x14, lr as x13 and r<n> as x<12-n>.
    
    Signed-off-by: João Peixoto <joaopeixotooficial@gmail.com>

--- src/arch/armv8/aarch32/aborts.c
+++ src/arch/armv8/aarch32/aborts.c
@@ -7,11 +7,17 @@
 #include <arch/sysregs.h>
 #include <cpu.h>
 
+/**
+ * SAVE_HYP_GPRS pushes r0-r12 and then the pre-exception sp and lr, so gprs[0] is sp, gprs[1] is
+ * lr and gprs[2 + n] is r<n>.
+ */
 void internal_abort_handler(unsigned long gprs[])
 {
-    for (ssize_t i = 14; i >= 0; i--) {
-        console_printk("x%ld:\t\t0x%0lx\n", i, gprs[14 - i]);
+    for (size_t i = 0; i < 13; i++) {
+        console_printk("r%zu:\t\t0x%0lx\n", i, gprs[2 + i]);
     }
+    console_printk("SP:\t\t0x%0lx\n", gprs[0]);
+    console_printk("LR:\t\t0x%0lx\n", gprs[1]);
     console_printk("ESR:\t0x%0lx\n", sysreg_esr_el2_read());
     console_printk("ELR:\t0x%0lx\n", sysreg_elr_el2_read());
     console_printk("FAR:\t0x%0lx\n", sysreg_far_el2_read());
